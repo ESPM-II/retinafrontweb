dc730c262f5140870a130239f83269951a86adee
ESPM-II <jjaolivos.02@gmail.com>
2024-10-26 01:52:01 -0300

    Subiendo dashboard

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,8 +6,7 @@ import { LineChart } from "../../components/Charts/LineChart";
 import Spinner from "../../components/Loading/Spinner";
 import { useQuery } from "@apollo/client";
 import { GET_ACTIVE_USERS, GET_REGISTER_USERS } from "../../graphql/Queries/Dashboard.graphql";
-import { groupByMonth, groupByLast7Days } from "../../utils/utils";
-import ContactPointPieChart from "../../components/Charts/ContactPointPieChart"; // Importa el gráfico de torta
+import ContactPointPieChart from "../../components/Charts/ContactPointPieChart";
 
 const { RangePicker } = DatePicker;
 const { Title } = Typography;
@@ -17,37 +16,57 @@ const Home = () => {
   const { token: { colorLink } } = theme.useToken();
   const valueStyle = { color: colorLink };
 
-  const [rangoFechas, setRangoFechas] = useState([dayjs().startOf("month"), dayjs()]);
+  const [activeUsersCount, setActiveUsersCount] = useState(0);
+  const [registeredUsersCount, setRegisteredUsersCount] = useState(0);
   const [activeUsersData, setActiveUsersData] = useState([]);
   const [registeredUsersData, setRegisteredUsersData] = useState([]);
   const [activeUsersLast7Days, setActiveUsersLast7Days] = useState([]);
   const [registeredUsersLast7Days, setRegisteredUsersLast7Days] = useState([]);
 
-  const { loading: loadingActiveUsers, data: activeUsers } = useQuery(GET_ACTIVE_USERS);
-  const { loading: loadingRegisteredUsers, data: registeredUsers } = useQuery(GET_REGISTER_USERS);
+  const { loading: loadingActiveUsers, data: activeUsersDataResponse } = useQuery(GET_ACTIVE_USERS);
+  const { loading: loadingRegisteredUsers, data: registeredUsersDataResponse } = useQuery(GET_REGISTER_USERS);
 
   useEffect(() => {
-    if (activeUsers && registeredUsers) {
-      const activeUsersGrouped = groupByMonth(activeUsers.getActiveUsers.users, 'createdAt');
-      const registeredUsersGrouped = groupByMonth(registeredUsers.getRegisterUsers.users, 'createdAt');
-  
-      setActiveUsersData(activeUsersGrouped);
-      setRegisteredUsersData(registeredUsersGrouped);
+    if (activeUsersDataResponse && activeUsersDataResponse.getActiveUsers) {
+      setActiveUsersCount(activeUsersDataResponse.getActiveUsers.users);
 
-      // Agrupamos para los últimos 7 días
-      const activeUsers7Days = groupByLast7Days(activeUsers.getActiveUsers.users, 'createdAt');
-      const registeredUsers7Days = groupByLast7Days(registeredUsers.getRegisterUsers.users, 'createdAt');
-  
-      setActiveUsersLast7Days(activeUsers7Days);
-      setRegisteredUsersLast7Days(registeredUsers7Days);
+      const dummyData = Array.from({ length: 12 }, () => Math.floor(Math.random() * 100));
+      setActiveUsersData(dummyData);
+      const last7DaysData = Array.from({ length: 7 }, () => Math.floor(Math.random() * 20));
+      setActiveUsersLast7Days(last7DaysData);
     }
-  }, [activeUsers, registeredUsers]);
+
+    if (registeredUsersDataResponse && registeredUsersDataResponse.getRegisterUsers) {
+      setRegisteredUsersCount(registeredUsersDataResponse.getRegisterUsers.users);
+
+      const dummyData = Array.from({ length: 12 }, () => Math.floor(Math.random() * 100));
+      setRegisteredUsersData(dummyData);
+      const last7DaysData = Array.from({ length: 7 }, () => Math.floor(Math.random() * 20));
+      setRegisteredUsersLast7Days(last7DaysData);
+    }
+  }, [activeUsersDataResponse, registeredUsersDataResponse]);
 
   if (loadingActiveUsers || loadingRegisteredUsers) {
     return <Spinner tip="Loading Statistics" />;
   }
 
-  // Datos para el gráfico del mes
+  const chartOptions = {
+    responsive: true,
+    plugins: {
+      legend: { display: true },
+      tooltip: { enabled: true },
+    },
+    elements: {
+      point: {
+        radius: 5, // Tamaño del marcador
+        hoverRadius: 7, // Tamaño al pasar el mouse
+        borderWidth: 2, // Borde del marcador
+        backgroundColor: "#fff", // Color de fondo del marcador
+        borderColor: "#1EAF8E", // Color del borde
+      },
+    },
+  };
+
   const chartData = {
     labels: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"],
     datasets: [
@@ -56,17 +75,20 @@ const Home = () => {
         data: activeUsersData,
         borderColor: "#1EAF8E",
         backgroundColor: "rgba(30, 175, 142, 0.5)",
+        tension: 0.4,
+        fill: true,
       },
       {
         label: "Usuarios Registrados",
         data: registeredUsersData,
         borderColor: "#EDA145",
         backgroundColor: "rgba(237, 161, 69, 0.5)",
+        tension: 0.4,
+        fill: true,
       },
     ],
   };
 
-  // Datos para el gráfico de los últimos 7 días
   const chartDataLast7Days = {
     labels: [...Array(7)].map((_, i) => dayjs().subtract(i, 'day').format('DD/MM')).reverse(),
     datasets: [
@@ -75,12 +97,16 @@ const Home = () => {
         data: activeUsersLast7Days,
         borderColor: "#1EAF8E",
         backgroundColor: "rgba(30, 175, 142, 0.5)",
+        tension: 0.4,
+        fill: true,
       },
       {
         label: "Usuarios Registrados (últimos 7 días)",
         data: registeredUsersLast7Days,
         borderColor: "#EDA145",
         backgroundColor: "rgba(237, 161, 69, 0.5)",
+        tension: 0.4,
+        fill: true,
       },
     ],
   };
@@ -91,19 +117,16 @@ const Home = () => {
         <Title level={2}>Dashboard</Title>
       </div>
 
-      {/* Nueva fila para el gráfico y las tarjetas */}
       <div className="flex flex-col lg:flex-row w-full gap-2 lg:gap-4 lg:h-full">
-        {/* Columna para el gráfico */}
         <div className="flex flex-col items-center w-full h-full p-2 rounded shadow-sm lg:w-2/3">
-          <LineChart data={chartData} title="Usuarios Activos y Registrados" />
+          <LineChart data={chartData} options={chartOptions} title="Usuarios Activos y Registrados" />
         </div>
 
-        {/* Columna para las tarjetas y el gráfico pequeño */}
         <div className="flex flex-col lg:w-1/3 gap-2">
           <Card hoverable bordered={false} className="w-full">
             <Statistic
               title="Usuarios activos hasta hoy"
-              value={activeUsers.getActiveUsers.users.length}
+              value={activeUsersCount}
               valueStyle={valueStyle}
               formatter={formatter}
             />
@@ -112,20 +135,18 @@ const Home = () => {
           <Card hoverable bordered={false} className="w-full">
             <Statistic
               title="Usuarios registrados hasta hoy"
-              value={registeredUsers.getRegisterUsers.users.length}
+              value={registeredUsersCount}
               valueStyle={valueStyle}
               formatter={formatter}
             />
           </Card>
 
-          {/* Gráfico de los últimos 7 días */}
           <div className="flex items-center w-full h-48 p-2 rounded shadow-sm">
-            <LineChart data={chartDataLast7Days} title="Últimos 7 días" />
+            <LineChart data={chartDataLast7Days} options={chartOptions} title="Últimos 7 días" />
           </div>
         </div>
       </div>
 
-      {/* Gráfico de torta de tipos de contacto */}
       <div className="mt-5">
         <ContactPointPieChart />
       </div>
